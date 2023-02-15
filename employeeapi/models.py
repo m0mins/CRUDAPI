80c8c65f114cd76d127782545560b291c3775938
m0mins <momin.pstu.cse11.bd@gmail.com>
2023-02-16 02:47:00 +0600

    added image field

--- employeeapi/models.py
+++ employeeapi/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+def nameFile(instance, filename):
+     return '/'.join(['images', filename])
+    # return '/'.join(['images', str(instance.name), filename])
+
 class Employee(models.Model):
     fullname=models.CharField(max_length=100)
     emp_code=models.CharField(max_length=5)
     mobile=models.CharField(max_length=15)
+    image = models.ImageField(upload_to=nameFile, blank=True, null=True)
--- employeeapi/viewsets.py
+++ employeeapi/viewsets.py
@@ -5,4 +5,10 @@ from . import serializers
 class EmployeeViewset(viewsets.ModelViewSet):
     queryset = models.Employee.objects.all()
     serializer_class = serializers.EmployeeSerializer
+
+
+    def post(self, request, *args, **kwargs):
+        file = request.data['file']
+        image = UploadImageTest.objects.create(image=file)
+        return HttpResponse(json.dumps({'message': "Uploaded"}), status=200)
     
