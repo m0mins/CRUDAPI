6ea050380f63c2ad84babb39a55ed8bc06b1a314
m0mins <momin.pstu.cse11.bd@gmail.com>
2023-02-17 15:13:44 +0600

    added random_number filed for auto generate

--- employeeapi/models.py
+++ employeeapi/models.py
@@ -6,7 +6,9 @@ def nameFile(instance, filename):
 
 class Employee(models.Model):
     fullname=models.CharField(max_length=100)
-    emp_code=models.CharField(max_length=5)
+    emp_code=models.CharField(max_length=10)
+    # emp_code=models.SlugField(max_length=255)
     mobile=models.CharField(max_length=15)
+    random_number=models.SlugField(max_length=255)
     image = models.ImageField(upload_to=nameFile, blank=True, null=True)
     
--- employeeapi/serializers.py
+++ employeeapi/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 from . models import Employee
 from rest_framework.validators import UniqueValidator
+from coolname import generate_slug
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    random_number = serializers.CharField(
+        min_length=3, 
+        max_length=25, 
+        validators=[UniqueValidator(queryset=Employee.objects.all())], 
+        allow_null=True, 
+        default=None
+    )
     class Meta:
         model=Employee
-        fields=('fullname','emp_code','mobile','image')
+        fields=('fullname','emp_code','mobile','random_number','image')
         extra_kwargs = {
                 'mobile': {
                     'validators': [
@@ -15,4 +23,6 @@ class EmployeeSerializer(serializers.ModelSerializer):
                         )
                     ]
                 }
-            }           
+            }
+    def validate_random_number(self, val):
+        return val or generate_slug()           
